cbbf2fdd6c77b560f58d5f5857a968c43ef19cad
pablo_collazo <131548922+PabloFxdxdxd@users.noreply.github.com>
2026-03-24 12:16:04 -0600

    Cambios Iniciales generales
    - Pantalla Login
    - paths de assets (imagenes)
    - paths de assets (iconos)

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'SEKI',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,7 +29,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8DEAE6)),
+        
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -54,69 +57,155 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final colors = Theme.of(context).colorScheme;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: colors.secondary,
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+        
+        child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30), //Margen a la izquierda y derecha
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          mainAxisAlignment: MainAxisAlignment.center, //Centrar verticalmente
+          crossAxisAlignment: CrossAxisAlignment.start, //Alineación a la izquierda
+
+          children: [
+            //Titulo
+            Text("Iniciar sesión", style: TextStyle(
+              fontSize: 32, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.white)),
+            const SizedBox(height: 40), //separa los elementos con un espacio
+            
+            //Campo de Correo
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+                "Correo electrónico", 
+                style: TextStyle(
+
+                  color: Colors.white,
+                  fontSize: 15
+                )
             ),
+            const SizedBox(height: 8),
+            TextField(
+
+                decoration: InputDecoration(
+                hintText: "name@example.com",
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+
+              ),
+            ),
+            
+            const SizedBox(height: 20),
+            
+          //Campo de contraseña
+
+            Text(
+                "Contraseña", 
+                style: TextStyle(
+
+                  color: Colors.white,
+                  fontSize: 15
+                )
+            ),
+            const SizedBox(height: 8),
+            TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                hintText: "Introduce tu contraseña",
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            //Botón de inicio
+            Center( 
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colors.inversePrimary, 
+                  minimumSize: Size(double.infinity, 50), // Botón ancho
+                ),
+                child: Text("Entrar"),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Center(
+              child: Text(
+                "O",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15
+                )
+               ),
+            ),
+
+            const SizedBox(height: 20),
+
+            //Otras opciones de inicio
+
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white, 
+                  minimumSize: Size(double.infinity, 50),
+                  
+                ),
+                icon: Image.asset('assets/icons/google.png', height: 24),
+                label: Text(
+                  "Continuar con Google",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15
+                )
+              )
+            )
+            ),
+          
+           const SizedBox(height: 30),
+
+           //Para registrarse
+
+            Text(
+              "¿No tienes Cuenta?",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15
+              )
+              ),
+            
+            const SizedBox(height: 10),
+            Center( 
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: colors.inversePrimary, 
+                  minimumSize: Size(double.infinity, 50), 
+                ),
+                child: Text("¡Registrarse!"),
+              ),
+            )
+
+
+
           ],
         ),
+      )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
